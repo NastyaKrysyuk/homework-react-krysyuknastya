024abe86227cd781e7e244b342e0d77b82be762f
Nastya <nkrysyuk@inbox.ru>
2022-06-27 14:42:53 +0300

    update render()

--- src/books/books.tsx
+++ src/books/books.tsx
@@ -53,7 +53,6 @@ export default class Books extends React.Component<TProps, TState> {
       this.setState({ ...this.state, page: 1 })
       this.getBooks();
     }
-
   }
 
   render() {
@@ -69,22 +68,20 @@ export default class Books extends React.Component<TProps, TState> {
             !isLoading &&
             books.map((el: Book) => (
               <ListGroup.Item as="li" key={el.isbn}>
-               <span className="title">{el.name} </span> <span>{`Country: ${el.country}`}</span>
+                <span className="title">{el.name} </span> <span className="prop">{`Country: ${el.country}`}</span>
               </ListGroup.Item>
             ))}
         </ListGroup>
-
-        {books && !isError && 
-          <ButtonGroup  className="books-nav" aria-label="Basic example">
+        {books && !isError &&
+          <ButtonGroup className="books-nav" aria-label="Basic example">
             <Button
-            variant="secondary"
+              variant="secondary"
               disabled={this.state.page === 1}
-              onClick={(_e: any) => this.handlerPage("left")}
-            >
+              onClick={(_e: any) => this.handlerPage("left")}>
               left
             </Button>
             <Button
-            variant="secondary"
+              variant="secondary"
               // disabled={this.state.books.length < this.state.pageSize}
               onClick={(_e: any) => this.handlerPage("right")}>
               right
@@ -94,9 +91,4 @@ export default class Books extends React.Component<TProps, TState> {
       </div>
     )
   }
-
-
-
-
-
 }
